2ecc82f958aa3b872fbf4ad1a20c561b7472c5a9
prembhosalencc-blip <prembhosalencc@gmail.com>
2025-09-16 15:52:49 +0530

    Add files via upload
    Initial commit for email scam detector project

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import joblib
+
+model = joblib.load(r"C:\Users\Prem Bhosale\OneDrive\Documents\project of ml\spam_model.pkl")
+vectorizer = joblib.load(r"C:\Users\Prem Bhosale\OneDrive\Documents\project of ml\vectorizer.pkl")
+
+st.title("Email Scam Detector")
+
+email_text = st.text_area("Enter email text")
+
+if st.button("Check"):
+    if email_text.strip():
+        X = vectorizer.transform([email_text])
+        prediction = model.predict(X)[0]
+        result = "🚨 Scam / Spam" if prediction == 1 else "✅ Safe / Not Spam"
+        st.write(result)
+    else:
+        st.write("Please enter some text")
